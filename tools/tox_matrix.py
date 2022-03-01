69f4e41c95473bafc8f01010d2ecc6258d577e74
Conor MacBride <conor@macbride.me>
2022-03-01 01:55:38 +0000

    change env for sh

--- tools/tox_matrix.py
+++ tools/tox_matrix.py
@@ -93,7 +93,7 @@ def get_matrix_item(env, global_libraries, global_posargs, global_toxargs, globa
     if pytest:
         if platform == "windows":
             item["pytest_flag"] = (r"--junitxml=junit\test-results.xml "
-                                   r"--cov-report=xml:${Env:GITHUB_WORKSPACE}\coverage.xml")
+                                   r"--cov-report=xml:${GITHUB_WORKSPACE}\coverage.xml")
         else:
             item["pytest_flag"] = (r"--junitxml=junit/test-results.xml "
                                    r"--cov-report=xml:${GITHUB_WORKSPACE}/coverage.xml")
