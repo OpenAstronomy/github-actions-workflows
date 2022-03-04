11902974d4fea9d1c85b2ff6e2c5d7c0c1999939
Conor MacBride <conor@macbride.me>
2022-03-04 23:30:03 +0000

    Add tox, conda, coverage, display

--- tools/tox_matrix.py
+++ tools/tox_matrix.py
@@ -10,16 +10,20 @@ MACHINE_TYPE = {
     "windows": "windows-2019",
 }
 
-DEFAULT_PYTHON = "3.x"
-
 
 @click.command()
 @click.option("--envs", default="")
 @click.option("--libraries", default="")
 @click.option("--posargs", default="")
+@click.option("--toxdeps", default="")
 @click.option("--toxargs", default="")
-@click.option("--pytest", default="")
-def load_tox_targets(envs, libraries, posargs, toxargs, pytest):
+@click.option("--pytest", default="true")
+@click.option("--coverage", default="")
+@click.option("--conda", default="auto")
+@click.option("--display", default="false")
+@click.option("--default-python", default="")
+def load_tox_targets(envs, libraries, posargs, toxdeps, toxargs, pytest, coverage,
+                     conda, display, default_python):
     """Script to load tox targets for GitHub Actions workflow."""
     # Load envs config
     envs = yaml.load(envs, Loader=yaml.BaseLoader)
@@ -37,12 +41,22 @@ def load_tox_targets(envs, libraries, posargs, toxargs, pytest):
         global_libraries.update(libraries)
     print(json.dumps(global_libraries, indent=2))
 
+    # Default string parameters which can be overwritten by each env
+    string_parameters = {
+        "posargs": posargs,
+        "toxdeps": toxdeps,
+        "toxargs": toxargs,
+        "coverage": coverage,
+        "conda": conda,
+        "display": display,
+    }
+
     # Create matrix
     matrix = {"include": []}
     for env in envs:
         matrix["include"].append(
-            get_matrix_item(env, global_libraries=global_libraries, global_posargs=posargs,
-                            global_toxargs=toxargs, global_pytest=pytest)
+            get_matrix_item(env, global_libraries=global_libraries, global_string_parameters=string_parameters,
+                            global_pytest=pytest, default_python=default_python)
         )
 
     # Output matrix
@@ -50,22 +64,23 @@ def load_tox_targets(envs, libraries, posargs, toxargs, pytest):
     print(f"::set-output name=matrix::{json.dumps(matrix)}")
 
 
-def get_matrix_item(env, global_libraries, global_posargs, global_toxargs, global_pytest):
+def get_matrix_item(env, global_libraries, global_string_parameters, global_pytest, default_python):
 
-    # define spec for each matrix include
+    # define spec for each matrix include (+ global_string_parameters)
     item = {
         "os": None,
         "toxenv": None,
         "python_version": None,
         "name": None,
         "pytest_flag": None,
-        "toxargs": None,
-        "posargs": None,
         "libraries_brew": None,
         "libraries_brew_cask": None,
         "libraries_apt": None,
         "libraries_choco": None,
     }
+    for string_param, default in global_string_parameters.items():
+        env_value = env.get(string_param)
+        item[string_param] = default if env_value is None else env_value
 
     # set os and toxenv
     for k, v in MACHINE_TYPE.items():
@@ -81,7 +96,7 @@ def get_matrix_item(env, global_libraries, global_posargs, global_toxargs, globa
         major, minor = m.groups()
         item["python_version"] = f"{major}.{minor}"
     else:
-        item["python_version"] = DEFAULT_PYTHON
+        item["python_version"] = default_python
 
     # set name
     item["name"] = env.get("name", False) or item["toxenv"]
@@ -100,14 +115,6 @@ def get_matrix_item(env, global_libraries, global_posargs, global_toxargs, globa
     else:
         item["pytest_flag"] = ""
 
-    # set toxargs
-    env_toxargs = env.get("toxargs")
-    item["toxargs"] = global_toxargs if env_toxargs is None else env_toxargs
-
-    # set posargs
-    env_posargs = env.get("posargs")
-    item["posargs"] = global_posargs if env_posargs is None else env_posargs
-
     # set libraries
     env_libraries = env.get("libraries")
     libraries = global_libraries if env_libraries is None else env_libraries
@@ -115,6 +122,18 @@ def get_matrix_item(env, global_libraries, global_posargs, global_toxargs, globa
     item["libraries_brew_cask"] = " ".join(libraries.get("brew_cask", []))
     item["libraries_apt"] = " ".join(libraries.get("apt", []))
     item["libraries_choco"] = " ".join(libraries.get("choco", []))
+
+    # set "auto" conda value
+    if item["conda"] == "auto":
+        item["conda"] = "true" if "conda" in item["toxenv"] else "false"
+
+    # inject toxdeps for conda
+    if item["conda"] == "true" and "tox-conda" not in item["toxdeps"].lower():
+        item["toxdeps"] = ("tox-conda " + item["toxdeps"]).strip()
+
+    # verify values
+    assert item["conda"] in {"true", "false"}
+    assert item["display"] in {"true", "false"}
 
     return item
 
